17bb391c30352c9989efb61ecf33715c4290182a
Aleksiej Mitrofanov <aleksiej_mitrofanov@epam.com>
2018-08-06 18:54:53 +0300

    syntax fixes at ddl

--- database/migration/0.0.0/catalog.sql
+++ database/migration/0.0.0/catalog.sql
@@ -2,7 +2,7 @@ CREATE TABLE catalog_category (
         id bigserial NOT NULL,
         name varchar(128) NOT NULL,
         description text NOT NULL,
-        parent_category bigserial,
+        parent_id bigserial,
         PRIMARY KEY(id),
         FOREIGN KEY (parent_id) REFERENCES catalog_category(id)
 );
@@ -13,16 +13,16 @@ CREATE TABLE catalog_filter_type (
     -- add filter types and range constraints. May be regexp mask. 
     -- value name/ value type/constraint
     -- need to think further
-    PRIMARY KEY(id),
-)
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE catalog_filter (
         id bigserial NOT NULL,
         filter_name varchar(128) NOT NULL,
-        filter_type bigserial NOT NULL,
+        filter_type_id bigserial NOT NULL,
         description text,
         PRIMARY KEY(id),
-        FOREIGN KEY (filter_type_id) REFERENCES filter_type(id)
+        FOREIGN KEY (filter_type_id) REFERENCES catalog_filter_type(id)
 );
 
 
@@ -30,9 +30,7 @@ CREATE TABLE catalog_filter_to_category_mapping (
         filter_id bigserial NOT NULL,
         category_id bigserial NOT NULL,
         description text,
-        PRIMARY KEY(filter_id, catalog_id),
+        PRIMARY KEY(filter_id, category_id),
         FOREIGN KEY (filter_id) REFERENCES catalog_filter(id),
         FOREIGN KEY (category_id) REFERENCES catalog_category(id)
 );
-
-
