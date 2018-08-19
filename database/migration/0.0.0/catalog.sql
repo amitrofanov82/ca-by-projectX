7bb381a0a0b359a84484bb855861ce251c472893
Aliaksandr Akulchyk <AAkulchyk@luxoft.com>
2018-08-19 15:19:24 +0200

    modified sql and liquebase

--- database/migration/0.0.0/catalog.sql
+++ database/migration/0.0.0/catalog.sql
@@ -1,36 +1,26 @@
-CREATE TABLE catalog_category (
-        id bigserial NOT NULL,
-        name varchar(128) NOT NULL,
-        description text NOT NULL,
-        parent_id bigserial,
-        PRIMARY KEY(id),
-        FOREIGN KEY (parent_id) REFERENCES catalog_category(id)
-);
-
-CREATE TABLE catalog_filter_type (
+CREATE TABLE labels (
     id bigserial NOT NULL,
-    name varchar(64),
-    -- add filter types and range constraints. May be regexp mask. 
-    -- value name/ value type/constraint
-    -- need to think further
+    name varchar(128) NOT NULL,
+    parent_id bigserial,
+    is_category boolean SET DEFAULT true,
+    description text,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE catalog_filter (
-        id bigserial NOT NULL,
-        filter_name varchar(128) NOT NULL,
-        filter_type_id bigserial NOT NULL,
-        description text,
-        PRIMARY KEY(id),
-        FOREIGN KEY (filter_type_id) REFERENCES catalog_filter_type(id)
+CREATE TABLE products (
+    id bigserial NOT NULL,
+    name varchar(128) NOT NULL,
+    description text,
+    shop_id bigserial NOT NULL,
+    price numeric,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (shop_id) REFERENCES shops(id)
 );
 
-
-CREATE TABLE catalog_filter_to_category_mapping (
-        filter_id bigserial NOT NULL,
-        category_id bigserial NOT NULL,
-        description text,
-        PRIMARY KEY(filter_id, category_id),
-        FOREIGN KEY (filter_id) REFERENCES catalog_filter(id),
-        FOREIGN KEY (category_id) REFERENCES catalog_category(id)
+CREATE TABLE product_images (
+    id bigserial NOT NULL,
+    product_id bigserial NOT NULL,
+    image_link text NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
