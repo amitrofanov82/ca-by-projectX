6c9e3f28091fca382bff81f7e58eb845a7f8f3ee
Aleksiej Mitrofanov <aleksiej_mitrofanov@epam.com>
2018-08-22 22:51:19 +0300

    Swagger, Swagger UI, dev home page, documentation

--- src/main/java/ca/by/project_x/web/controllers/rest/CatalogController.java
+++ src/main/java/ca/by/project_x/web/controllers/rest/CatalogController.java
@@ -1,4 +1,4 @@
-package ca.by.project_x.web.controllers;
+package ca.by.project_x.web.controllers.rest;
 
 
 import java.util.ArrayList;
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,13 @@ import ca.by.project_x.dto.catalog.Category;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v2/catalog")
 @Slf4j
-public class RootController {
+public class CatalogController {
 
-
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Category>> getCatalog() {
-        log.info("new main page request");
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, path="/category")
+    public ResponseEntity<List<Category>> getAllCategories() {
+        log.debug("get catalog method called");
         List<Category> catalog = new ArrayList<>(3);
         Category cat1 = new Category(null,new ArrayList<>(),"Тапки","Всяки разны тапки, от домашних до похоронных");
         Category cat2 = new Category(null,new ArrayList<>(),"Деньги","Здесь вы можете купить себе ещё больше денег");
@@ -35,6 +36,20 @@ public class RootController {
         catalog.addAll(Arrays.asList(new Category[] {cat1, cat2, cat3}));
         
         return new ResponseEntity<>(catalog, HttpStatus.OK);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, path="/category")
+    public ResponseEntity<String> addCategory(@RequestBody Category newCategory) {
+        log.debug("new category add request");
+        System.out.println(newCategory.toString());
+        return new ResponseEntity<>("{\"result\":\"success\"}", HttpStatus.OK);
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE, path="/category/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        log.debug("new category add request");
+        System.out.println("delete categor with id = " + id);
+        return new ResponseEntity<>("{\"result\":\"success\"}", HttpStatus.OK);
     }
 
 }
