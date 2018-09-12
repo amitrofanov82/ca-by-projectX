eeb380f05b6076d9bb7047edaa42ad3bfe95240a
amitrofanov82 <no@mail.com>
2018-09-12 23:30:38 +0300

    bat script fix

--- startCoolProject.bat
+++ startCoolProject.bat
@@ -2,6 +2,6 @@ REM uncomment to use postgre db profile
 REM mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=devpg"
 
 REM uncomment to use h2 in memory db
-mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=devh2"
+mvn spring-boot:run -Dspring-boot.run.profiles=devh2
 
 
