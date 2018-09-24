6ec154c863d1fd658d397282506bb4dbea8baf90
deadletter <ales.akulchyk@gmail.com>
2018-09-24 21:43:05 +0200

    Squashed commit of the following:
    commit fb3e1f5d124181b2cf7bd8cc3599f941f96e4b90
    Author: deadletter <ales.akulchyk@gmail.com>
    Date:   Mon Sep 10 22:59:43 2018 +0200
    
        added some test data

--- src/main/resources/db/changelog/0.0.1/db-test-data.sql
+++ src/main/resources/db/changelog/0.0.1/db-test-data.sql
@@ -1,2 +1,7 @@
-INSERT INTO USERS(id,nickname)  VALUES  (12,'Kolya');
+INSERT INTO USERS(id,nickname, email)  VALUES  (12,'Kolya Vasin', 'kolya@mail.ru');
+INSERT INTO USERS(id,nickname, email)  VALUES  (13,'Vasya Petin', 'vasya@gmail.com');
+INSERT INTO USERS(id,nickname, email)  VALUES  (14,'Petya Kolin', 'petya-kolin@narod.ru');
+INSERT INTO USERS(id,nickname, email)  VALUES  (15,'Ivan Drago', 'ivan.drago@kgb.ru');
+INSERT INTO USERS(id,nickname)  VALUES  (16,'Alyosha');
+
 INSERT INTO SHOPS(id, admin_id)  VALUES  (12,12);
